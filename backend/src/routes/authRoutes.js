dc1d66d454e0009e64cda2a001b7a809927e4bfe
Hussnain Cheema <cheemahussnain155@gmail.com>
2025-11-22 11:47:54 +0500

    first commit

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/register", async(req, res) => {
+  res.send("register");
+});
+
+router.get("/login", async(req, res) => {
+  res.send("login");
+});
+
+router.get("/login", async(req, res) => {
+  res.send("login");
+});
+export default router;
